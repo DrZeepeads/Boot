764493a8ef83f8cb6a067383f5d774cd43eb568e
nelson <175583803+DrZeepeads@users.noreply.github.com>
2024-12-25 18:06:16 +0500

    Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer bg-light text-center py-3">
+      <p>© 2024 NelsonBot. All rights reserved.</p>
+    </footer>
+  );
+}
